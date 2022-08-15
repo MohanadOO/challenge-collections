d7f3b419c3d5bc92d2087d58ff50954de9d0aeb0
MohanadOO <moohand220@gmail.com>
2022-08-15 22:22:12 +0300

    Day 3 (LeetCode Programming Skills II)

---
+++ LeetCode Study Plan/Programming Skills II (LeetCode)/Complete/150_EvaluateReversePolishNotaiton.ts
@@ -0,0 +1,35 @@
+//https://leetcode.com/problems/evaluate-reverse-polish-notation/
+//Medium
+
+function evalRPN(tokens: string[]): number {
+  let currNumbers: string[] = []
+
+  for (let i = 0; i < tokens.length; i++) {
+    if (tokens[i].match(/[+-/*]$/g)) {
+      const lastNumber = Number(currNumbers[currNumbers.length - 1])
+      const secondsLastNumber = Number(currNumbers[currNumbers.length - 2])
+      const operator = tokens[i]
+
+      currNumbers.splice(
+        currNumbers.length - 2,
+        2,
+        operation(secondsLastNumber, lastNumber, operator).toString()
+      )
+    } else currNumbers.push(tokens[i])
+  }
+
+  function operation(x: number, y: number, operator: string): number {
+    if (operator === '+') return x + y
+    else if (operator === '-') return x - y
+    else if (operator === '*') return x * y
+    else return x / y > 0 ? Math.floor(x / y) : Math.ceil(x / y)
+  }
+
+  return Number(currNumbers.join(''))
+}
+
+console.log(evalRPN(['2', '1', '+', '3', '*']))
+console.log(evalRPN(['4', '13', '5', '/', '+']))
+console.log(
+  evalRPN(['10', '6', '9', '3', '+', '-11', '*', '/', '*', '17', '+', '5', '+'])
+)
