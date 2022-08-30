a72b156b19fdfa1b6d9550862a0ab6bc23dd6467
MohanadOO <moohand220@gmail.com>
2022-08-30 06:27:54 +0300

    Day 15 (LeetCode Programming Skills II) ✅

---
+++ LeetCode Study Plan/Programming Skills II (LeetCode)/Complete/2_AddTwoNumbers.ts
@@ -0,0 +1,75 @@
+//https://leetcode.com/problems/add-two-numbers/
+//Medium
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  if (!l1 && !l2) return null
+  if (!l1) return l2
+  if (!l2) return l1
+
+  let current = new ListNode(0)
+  let result = current
+
+  let sum = 0
+  let carry = 0
+
+  while (l1 !== null || l2 !== null || sum > 0) {
+    if (l1 !== null) {
+      sum += l1.val
+      l1 = l1.next
+    }
+
+    if (l2 !== null) {
+      sum += l2.val
+      l1 = l2.next
+    }
+
+    if (sum >= 10) {
+      carry = 1
+      sum = sum - 10
+    }
+
+    result.next = new ListNode(sum)
+    result = result.next
+
+    sum = carry
+    carry = 0
+  }
+
+  return current.next
+}
+
+//This Solution Works
+function addTwoNumbers(
+  l1: ListNode | null,
+  l2: ListNode | null,
+  carryOver: number = 0
+): ListNode | null {
+  if (l1 === null && l2 === null) {
+    return carryOver === 0 ? null : new ListNode(carryOver)
+  }
+
+  const currentSum = (l1?.val || 0) + (l2?.val || 0) + carryOver
+  return new ListNode(
+    currentSum % 10,
+    addTwoNumbers(
+      l1?.next || null,
+      l2?.next || null,
+      (currentSum / 10) >> 0 // slightly faster than Math.floor(currentSum/10)
+    )
+  )
+}
---
+++ LeetCode Study Plan/Programming Skills II (LeetCode)/Complete/445_AddTwoNumbersII.ts
@@ -0,0 +1,52 @@
+//https://leetcode.com/problems/add-two-numbers-ii/
+//Medium
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+
+  let resultList: number[][] = [[], []]
+
+  while (l1 || l2) {
+    if (l1) {
+      resultList[0].push(l1.val)
+      l1 = l1.next
+    }
+    if (l2) {
+      resultList[1].push(l2.val)
+      l2 = l2.next
+    }
+  }
+
+  let result = new ListNode(0)
+
+
+  while(resultList[0].length || resultList[1].length){
+    let sum = 0;
+    if (resultList[0].length) sum += resultList[0].pop()
+    if (resultList[1].length) sum += resultList[1].pop()
+    
+    sum += result.val
+    result.val = sum % 10
+
+    let head = new ListNode(Math.floor(sum / 10))
+    head.next = result
+    result = head
+  }
+
+
+  return (result.val === 0) ? result.next : result
+}
