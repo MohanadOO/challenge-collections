a6f89c21ef9bbd4208f141fbef27da884824e3bc
MohanadOO <moohand220@gmail.com>
2022-11-01 09:47:12 +0300

    Edit & Update Challenge #150 ✅

--- LeetCode Study Plan/Programming Skills II (LeetCode)/Complete/150_EvaluateReversePolishNotaiton.ts
+++ LeetCode Study Plan/Programming Skills II (LeetCode)/Complete/150_EvaluateReversePolishNotaiton.ts
@@ -2,6 +2,11 @@
 //Medium
 
 function evalRPN(tokens: string[]): number {
+  return evalRPNMySolution(tokens)
+  return evalRPNStack(tokens)
+}
+
+function evalRPNMySolution(tokens: string[]): number {
   let currNumbers: string[] = []
 
   for (let i = 0; i < tokens.length; i++) {
@@ -9,7 +14,6 @@ function evalRPN(tokens: string[]): number {
       const lastNumber = Number(currNumbers[currNumbers.length - 1])
       const secondsLastNumber = Number(currNumbers[currNumbers.length - 2])
       const operator = tokens[i]
-
       currNumbers.splice(
         currNumbers.length - 2,
         2,
@@ -28,8 +32,28 @@ function evalRPN(tokens: string[]): number {
   return Number(currNumbers.join(''))
 }
 
+function evalRPNStack(tokens: string[]): number {
+  let a: number | undefined, b: number | undefined
+  const evaluate = {
+    '+': () => a + b,
+    '-': () => a - b,
+    '*': () => a * b,
+    '/': () => ~~(a / b),
+  }
+  const stack: number[] = []
+
+  for (let t of tokens) {
+    if (evaluate[t]) {
+      b = stack.pop()
+      a = stack.pop()
+      stack.push(evaluate[t]())
+    } else stack.push(~~t)
+  }
+  return stack[0]
+}
+
 console.log(evalRPN(['2', '1', '+', '3', '*']))
-console.log(evalRPN(['4', '13', '5', '/', '+']))
-console.log(
-  evalRPN(['10', '6', '9', '3', '+', '-11', '*', '/', '*', '17', '+', '5', '+'])
-)
+// console.log(evalRPN(['4', '13', '5', '/', '+']))
+// console.log(
+//   evalRPN(['10', '6', '9', '3', '+', '-11', '*', '/', '*', '17', '+', '5', '+'])
+// )
