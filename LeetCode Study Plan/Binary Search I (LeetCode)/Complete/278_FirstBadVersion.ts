86b72de4969c122a5760c684f32703b92685f514
MohanadOO <moohand220@gmail.com>
2022-11-30 17:11:55 +0300

    Day 5 (LeetCode Binary Search I) ✅

---
+++ LeetCode Study Plan/Binary Search I (LeetCode)/Complete/278_FirstBadVersion.ts
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/first-bad-version/?envType=study-plan&id=binary-search-i
+
+/**
+ * The knows API is defined in the parent class Relation.
+ * isBadVersion(version: number): boolean {
+ *     ...
+ * };
+ */
+
+function solution(isBadVersion: any) {
+  return (n: number): number => {
+    let start = 0
+    let end = n
+
+    while (start <= end) {
+      let middle = Math.floor((start + end + 1) / 2)
+      if (isBadVersion(middle)) end = middle - 1
+      else start = middle + 1
+    }
+    return start
+  }
+}
