989f9674a1d90cc55df8a588c4a6ff9cc4bb8203
MohanadOO <moohand220@gmail.com>
2023-02-18 06:58:07 +0300

    Day 18 Challenge (Feb 2023) 📅

---
+++ LeetCode Monthly/Feb (2023)/226.ts
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/invert-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+  if (root === null) return null
+
+  const right = invertTree(root.right)
+  const left = invertTree(root.left)
+
+  root.right = left
+  root.left = right
+
+  return root
+}
